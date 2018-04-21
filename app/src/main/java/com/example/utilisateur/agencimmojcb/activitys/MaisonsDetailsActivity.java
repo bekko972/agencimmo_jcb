83b46ed32f56739cdeddd0ebfa9e62e8a2223492
bekko972 <jcboulange972@gmail.com>
2018-04-20 22:14:32 -0400

    Affichage détails maisons OK Il manque quelques champs mais le principe est acquis

--- app/src/main/java/com/example/utilisateur/agencimmojcb/activitys/MaisonsDetailsActivity.java
+++ app/src/main/java/com/example/utilisateur/agencimmojcb/activitys/MaisonsDetailsActivity.java
@@ -1,18 +1,26 @@
 package com.example.utilisateur.agencimmojcb.activitys;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.utilisateur.agencimmojcb.R;
 import com.example.utilisateur.agencimmojcb.objets.Agent;
+import com.example.utilisateur.agencimmojcb.objets.Maison;
 
 public class MaisonsDetailsActivity extends AppCompatActivity {
     TextView prenomAgent;
+    TextView nomMaison;
+    TextView entiteMaison;
+    TextView lieuMaison;
+    TextView superficieMaison;
+    TextView typeMaison;
+    TextView prixMaison;
+
     //ListView listeMaisons;
     Agent agent = new Agent();
+    Maison maison = new Maison();
 
 
     @Override
@@ -21,7 +29,11 @@ public class MaisonsDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_maisons_details);
 
         prenomAgent = (TextView) findViewById(R.id.tv_pseudo_vue4);
+        nomMaison = (TextView) findViewById(R.id.tv_nomMaison_vue4);
+        entiteMaison = (TextView) findViewById(R.id.tv_entiteMaison_vue4);
+
         AfficherPrenomAgent();
+        AfficherMaisonSelectionnee();
     }
 
     protected void AfficherPrenomAgent()
@@ -38,6 +50,60 @@ public class MaisonsDetailsActivity extends AppCompatActivity {
                     prenomAgent.setText(String.format("%s",prenom));
                     agent.setPrenom(prenomAgent.getText().toString());
                 }
+            }
+        }
+    }
+
+    protected void AfficherMaisonSelectionnee()
+    {
+
+        Intent intent = getIntent();
+        if(intent!=null)
+        {
+            Bundle extras = intent.getExtras();
+            if(extras!=null)
+            {
+                String entite = extras.getString("entite");
+                if(entite!=null)
+                {
+                    entiteMaison.setText(String.format("%s",entite));
+                    maison.setNom(entiteMaison.getText().toString());
+                }
+
+                String nom = extras.getString("nom");
+                if(nom!=null)
+                {
+                    nomMaison.setText(String.format("%s",nom));
+                    maison.setNom(nomMaison.getText().toString());
+                }
+
+                /*String lieu = extras.getString("lieu");
+                if(lieu!=null)
+                {
+                    lieuMaison.setText(String.format("%s",lieu));
+                    maison.setLieu(lieuMaison.getText().toString());
+                }
+
+                String superficie = extras.getString("superficie");
+                if(superficie!=null)
+                {
+                    superficieMaison.setText(String.format("%s",superficie));
+                    maison.setSuperficie(Integer.parseInt(superficieMaison.getText().toString()));
+                }
+
+                String type = extras.getString("type");
+                if(type!=null)
+                {
+                    typeMaison.setText(String.format("%s",type));
+                    maison.setType(typeMaison.getText().toString());
+                }
+
+                String prix = extras.getString("prix");
+                if(prix!=null)
+                {
+                    prixMaison.setText(String.format("%s",prix));
+                    maison.setPrix(Integer.parseInt(prixMaison.getText().toString()));
+                }*/
             }
         }
     }
